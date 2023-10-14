13e34916060f09f0b2ff453fcba3d0eb78a53e44
Davi Paula <davipaula2005@gmail.com>
2023-10-14 16:00:56 -0300

    ..

--- codigo.R
+++ codigo.R
@@ -1,3 +1,7 @@
+# Importando pacotes
+
+library(tidyverse)
+
 # Definindo o tema para gráficos
 
 cores_estat <- c("#A11D21", "#003366", "#CC9900", "#663333", "#FF6600", "#CC9966", "#999966", "#006606", "#008091", "#041835", "#666666")
@@ -35,6 +39,56 @@ infantil <- vendas[vendas$Category == "Kids' Fashion", "Price"]
 
 # Calculando a renda anual de cada categoria
 
-sum(feminina, na.rm = T)
-sum(masculina, na.rm = T)
-sum(infantil, na.rm = T)
+rfem = sum(feminina, na.rm = T)
+rmas = sum(masculina, na.rm = T)
+rinf = sum(infantil, na.rm = T)
+
+# Repetindo o processo para os produtos que não foram devolvidos
+
+feminina2 <- vendas[vendas$Category == "Women's Fashion" & is.na(vendas$Motivo.devolução), "Price"]
+masculina2 <- vendas[vendas$Category == "Men's Fashion" & is.na(vendas$Motivo.devolução), "Price"]
+infantil2 <- vendas[vendas$Category == "Kids' Fashion" & is.na(vendas$Motivo.devolução), "Price"]
+
+rfem2 = sum(feminina2, na.rm = T)
+rmas2 = sum(masculina2, na.rm = T)
+rinf2 = sum(infantil2, na.rm = T)
+
+# Criando um novo dataframe para elaboração de gráficos
+
+Categorias <- c("Moda Feminina", "Moda Masculina", "Moda Infantil")
+Renda.Anual <- c(rfem, rmas, rinf)
+Freq.Rel <- c("33,24%", "31,49%", "35,27%")
+Renda.Anual2 <- c(rfem2, rmas2, rinf2)
+Freq.Rel2 <- c("32,97%", "30,91%", "36,12%")
+
+rendas_anuais <- data.frame(Categorias, Renda.Anual, Freq.Rel, Renda.Anual2, Freq.Rel2)
+
+# Primeiro gráfico de frequências
+
+label = str_c(Renda.Anual, " (", Freq.Rel, ")")
+RA1 <- ggplot(rendas_anuais) +
+  aes(x = Categorias, y = Renda.Anual, label = label) +
+  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, #hjust = .5,
+    size = 3
+  ) + 
+  labs(x = "Categoria", y = "Renda Anual") +
+  theme_estat()
+RA1
+
+# Segundo gráfico de frequências
+
+label2 = str_c(Renda.Anual2, " (", Freq.Rel2, ")")
+RA2 <- ggplot(rendas_anuais) +
+  aes(x = Categorias, y = Renda.Anual2, label = label2) +
+  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, #hjust = .5,
+    size = 3
+  ) + 
+  labs(x = "Categoria", y = "Renda Anual") +
+  theme_estat()
+RA2
